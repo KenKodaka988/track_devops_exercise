3a7545fe80abb2c5e3435c1e758eabf01e79f067
KenKodaka988 <ken.kodaka@tcs.com>
2026-06-12 12:02:01 +0900

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,13 @@
-def add(a, b, c=0):
-  try:
-    return int(a + b) + int(c)
-  except:
-    return "error"
+from src.main import add
+
+def test_add_1():
+    assert add(1, 2) == 3
+
+def test_add_2():
+    assert add(1, 2, 3) == 6
+
+def test_add_3():
+    assert add("1", "2") == 3
+
+def test_add_error():
+    assert add("a", 1) == "error"
